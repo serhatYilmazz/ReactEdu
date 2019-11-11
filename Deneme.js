14e220ecb0989bc5a7fa6ac1dcd437c9233d675e
Serhat <yilmazserhat030@gmail.com>
2019-11-11 17:02:48 +0300

    Delete Deneme.js

--- Deneme.js
+++
@@ -1,9 +0,0 @@
-function Blockchain() {
-    this.chain = [];
-    this.pendingTransactions = [];
-    this.networkNodes = [];
-}
-
-Blockchain.prototype.getLastBlock = function() {
-    return this.chain[this.chain.length - 1];
-};
